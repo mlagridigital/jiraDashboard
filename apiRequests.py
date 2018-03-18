8960ec30405491cfb6cf8de728059a1c433a92d3
timvan <timvanellemeet@gmail.com>
2018-03-19 10:49:24 +1100

    remodelled requests and progressing through formatting data

--- apiRequests.py
+++ apiRequests.py
@@ -1,63 +1,76 @@
 import requests
 import json
 
-def get_stories(sprint):
-	"""
-	TODO - get/request code is currently duplicated and can be reduced
+# def get_stories(sprint):
+# 	"""
+# 	TODO - get/request code is currently duplicated and can be reduced
 
 
-	"jql" : "project = ADS AND sprint = 'A-Team' AND sprint IN openSprints() AND sprint NOT IN futureSprints() AND type in standardIssueTypes()",
+# 	"jql" : "project = ADS AND sprint = 'A-Team' AND sprint IN openSprints() AND sprint NOT IN futureSprints() AND type in standardIssueTypes()",
 
-	"""
+# 	"""
 
-	url = 'https://fullprofile.atlassian.net/rest/api/2/search'
-				# '?jql=\
-				# project=ADS\
-				# +AND+sprint="A-Team"\
-				# +AND+sprint+IN+openSprints()\
-				# +AND+sprint+NOT+IN+futureSprints()\
-				# +AND+type+IN+standardIssueTypes()\
-				# '
+# 	url = 'https://fullprofile.atlassian.net/rest/api/2/search'
+# 				# '?jql=\
+# 				# project=ADS\
+# 				# +AND+sprint="A-Team"\
+# 				# +AND+sprint+IN+openSprints()\
+# 				# +AND+sprint+NOT+IN+futureSprints()\
+# 				# +AND+type+IN+standardIssueTypes()\
+# 				# '
 
 
-	querystring = {
-		"jql" : "project = ADS AND sprint = " + str(sprint) + " AND type in standardIssueTypes()",
-		"maxResults" : "100",
-		"fields" : "status, subtasks, issuetype, summary, timespent"
-	}
+# 	querystring = {
+# 		"jql" : "project = ADS AND sprint = " + str(sprint) + " AND type in standardIssueTypes()",
+# 		"maxResults" : "100",
+# 		"fields" : "status, subtasks, issuetype, summary, timespent"
+# 	}
 
-	headers = {
-	    'Authorization': "Basic dGltLnZhbi5lbGxlbWVldDpBZ3JpZGlnaXRhbDEhamlyYQ==",
-	    'Cache-Control': "no-cache",
-	    'Postman-Token': "9514aa40-4142-43df-bf5e-361c551463f2"
-	    }
+# 	headers = {
+# 	    'Authorization': "Basic dGltLnZhbi5lbGxlbWVldDpBZ3JpZGlnaXRhbDEhamlyYQ==",
+# 	    'Cache-Control': "no-cache",
+# 	    'Postman-Token': "9514aa40-4142-43df-bf5e-361c551463f2"
+# 	    }
 
-	response = requests.request("GET", url, headers=headers, params=querystring)
+# 	response = requests.request("GET", url, headers=headers, params=querystring)
 
-	parsed = response.json()
-	#print(json.dumps(parsed['issues'][0], indent = 4))
+# 	parsed = response.json()
+# 	#print(json.dumps(parsed['issues'][0], indent = 4))
 	
-	print("Requesting stories in sprint:", sprint)
-	print("Recieved:", len(parsed['issues']), "/", parsed['total'] ,"stories")
+# 	print("Requesting jql query: ", querystring['jql'])
+# 	print("Filtering for fields: ", querystring['fields'])
+# 	print("Recieved:", len(parsed['issues']), "/", parsed['total'] ,"stories")
 
 
-	return parsed['issues']
+# 	return parsed['issues']
 
 
-def get_subtasks(sprint):
+def get_issues(sprint, search = None):
 	"""
 	TODO -  request does not capture all the results, limited by maxResults. only maxResults = 100.
 			Explore pagination of results, or perform multiple requests starting at n * maxResults
 	
 	"""
-	
+		
 	url = 'https://fullprofile.atlassian.net/rest/api/2/search'
 
-	querystring = {
-		"jql" : "project = ADS AND sprint = " + str(sprint) + " AND type in subtaskIssueTypes()",
-		"maxResults" : "200",
-		"fields" : "status, issuetype"
-	}
+	if search == "stories":
+		querystring = {
+			"jql" : "project = ADS AND sprint = " + str(sprint) + " AND type in standardIssueTypes()",
+			"maxResults" : "100",
+			"startAt" : 0,
+			"fields" : "status, subtasks, issuetype, summary, aggregatetimespent, aggregatetimeoriginalestimate, aggregatetimeestimate"
+		}
+	elif search == "subtasks":
+		querystring = {
+			"jql" : "project = ADS AND sprint = " + str(sprint) + " AND type in subtaskIssueTypes()",
+			"maxResults" : "100",
+			"startAt" : 0,
+			"fields" : "status, issuetype"
+		}
+	else:
+		print("Error: invalid search criteria, only search for stories or subtasks")
+		return
 
 	headers = {
 	    'Authorization': "Basic dGltLnZhbi5lbGxlbWVldDpBZ3JpZGlnaXRhbDEhamlyYQ==",
@@ -65,14 +78,35 @@ def get_subtasks(sprint):
 	    'Postman-Token': "9514aa40-4142-43df-bf5e-361c551463f2"
 	    }
 
+	print("-" * 10)
+	print("Getting", search)
+	print("Requesting jql query: ", querystring['jql'])
+	print("Filtering for fields: ", querystring['fields'])
 	response = requests.request("GET", url, headers=headers, params=querystring)
 
 	parsed = response.json()
+
+	print("Recieved", len(parsed['issues']), "issues starting at", parsed['startAt'], "of a total of", parsed['total'])
+
+	# CHECK FOR CASE WHERE NUMBER OF TOTAL == MAXRESULTS
+	# SENSE CHECK DATA - COMPARE AGAINST
+	if parsed['total'] > parsed['maxResults']:
+
+		remaining_calls = parsed['total'] // parsed['maxResults']
+
+		for i in range(remaining_calls):
+
+			querystring['startAt'] = (i + 1) * parsed['maxResults']
+			response = requests.request("GET", url, headers=headers, params=querystring).json()
+			print("Recieved", len(response['issues']), "issues starting at", response['startAt'], "of a total of", response['total'])
+
+			for issue in response['issues']:
+				parsed['issues'].append(issue)
+
+
+	print("Received", len(parsed['issues']), "issues in total")
 	#print(json.dumps(parsed, indent = 4))
-
-	print("Requesting subtasks in sprint:", sprint)
-	print("Recieved:", len(parsed['issues']), "/", parsed['total'] ,"subtasks")
-
+	
 	return parsed['issues']
 
 
@@ -104,6 +138,24 @@ def create_lookup(parsed):
 
 
 def format_data(stories, subtasks):
+	"""
+	# FOR BURNDOWN - datetime is to show timespent on task over time
+
+	# TODO 
+		- ADD LOG TIME FIELDS TO STORIES / SUBTASKS; logtime = [developer, timespent, datetime];
+			log are split by using ;  split go from end
+			using datetime to format datetime fields
+			idea for error checking  - function should return correct type
+			check jira.tempo to which DEV??; expected none
+			check logs include comments; attention these look to have affected .csv
+	
+		- Add status to issues
+
+
+	# QUESTIONS 
+		- Include Priorities?? 
+		- For Defects, Bugs and Subtasks include ROOT CAUSE
+	"""
 
 	subtasksFormated = [{
 		"id" : int,
@@ -116,19 +168,10 @@ def format_data(stories, subtasks):
 		"timespent" : int,
 	}]
 
-	storiesFormated = [{
-		"id" : int,
-		"key" : "",
-		"self" : "",
-		"issuetype" : "",
-		"issuetypeIcon" :"",
-		"timeestimate" : int,
-		"remainingEstimate" : int,
-		"timespent" : int,
-		"subtasks" : subtasksFormated
-	}]
+	storiesFormated = []
 
-	# Support Issues are filtered and count and timeSpent is collected 
+
+	# Support Issues are filtered, total support issues are counted and timespent is summed
 	supportIssues = {
 		"count" : 0,
 		"timespent" : 0,
@@ -136,19 +179,51 @@ def format_data(stories, subtasks):
 
 
 	for i, issue in enumerate(stories):
+		
 		print("_" * 20)
-		print(issue['fields']['issuetype']['name'])
-		issue['fields']['issuetype']['name'] == "Support "
+		print("Issue Key:", issue['key'])
+		print("Issue type name:", issue['fields']['issuetype']['name'])
 
+
+		# Filter out support tasks
 		if issue['fields']['issuetype']['name'] == "Support ":
-			# supportIssues['count'] += 1
-			if isinstance(issue['fields']['timespent'], int):
-				supportIssues['timespent'] += issue['fields']['timespent']
-			pass
+			supportIssues['count'] += 1
+			if isinstance(issue['fields']['aggregatetimespent'], int):
+				supportIssues['timespent'] += issue['fields']['aggregatetimespent']
+			continue
+
+		# FILTER OUT 'TASKS'
+
+		# WHAT TO DO WITH TECH-DEBT?
+
+		# FORMAT self to url
+
+		newStory = {
+			"id" : issue['id'],
+			"key" : issue['key'],
+			"self" : "",
+			"issuetype" : issue['fields']['issuetype']['name'],
+			"issuetypeIcon" :"",
+			"aggregatetimespent" : issue['fields']['aggregatetimespent'],
+			"aggregatetimeoriginalestimate" : issue['fields']['aggregatetimeoriginalestimate'],
+			"aggregatetimeestimate" : issue['fields']['aggregatetimeestimate'],
+			"subtasks" : []
+		}
+
+		storiesFormated.append(newStory)
 
 		for subtask in issue['fields']['subtasks']:
-			# print(subtask['key'])
+			print("	Has subtasks:",subtask['key'])
 			subtasks.index
+
+	print(supportIssues)
+	print(json.dumps(storiesFormated, indent = 4))
+
+
+	print("_" * 50)
+	print(json.dumps(subtasks, indent = 4))
+
+
 
 
 	
@@ -158,8 +233,11 @@ if __name__ == "__main__":
 
 	sprint = 77
 
-	stories = get_stories(sprint)
-	subtasks = get_subtasks(sprint)
+	#stories = get_stories(sprint)
+	#subtasks = get_subtasks(sprint)
+
+	stories = get_issues(sprint, search = "stories")
+	subtasks = get_issues(sprint, search = "subtasks")
 	data = format_data(stories, subtasks)
 	#lookup = create_lookup(stories)
 	
